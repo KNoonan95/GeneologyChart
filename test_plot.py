084e05ba417b7e857b17418c45dc4b062f7bc047
KNoonan95 <Knoonan1995@gmail.com>
2025-06-07 22:24:27 +0100

    Update test_plot.py

--- test_plot.py
+++ test_plot.py
@@ -3,3 +3,7 @@ import os
 
 def test_plot_file_exists():
     assert os.path.exists('output/plot.html'), "Plot HTML file was not created."
+
+def test_plot_file_not_empty():
+    size = os.path.getsize('output/plot.html')
+    assert size > 5000, "Plot HTML file is too small, likely blank."
